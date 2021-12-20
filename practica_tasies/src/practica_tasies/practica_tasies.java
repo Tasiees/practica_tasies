a3a4321ab5584b1124f5eb4f3bede573b5551583
Tasiees <95215003+Tasiees@users.noreply.github.com>
2021-12-20 10:16:03 +0100

    HOLA BB

---
+++ practica_tasies/src/practica_tasies/practica_tasies.java
@@ -0,0 +1,19 @@
+package practica_tasies;
+
+public class practica_tasies {
+		public static void main(String[] args) {
+			printOne();
+			printOne();
+			printTwo();
+		}
+
+		public static void printOne() {
+			System.out.println("Hello World");
+		}
+
+		public static void printTwo() {
+			printOne();
+			printOne();
+		}
+	}
+
